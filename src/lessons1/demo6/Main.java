01f5e5e4428779c70b390c7670097a149e4aacac
SerhiiKI13 <kugai.sirozha@gmail.com>
2025-08-03 11:42:06 +0200

    lessons 1.6

---
+++ src/lessons1/demo6/Main.java
@@ -0,0 +1,13 @@
+package lessons1.demo6;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] arrays = {"a", "b", "c"};
+        for (String array : arrays) {
+            System.out.println(array);
+        }
+        System.out.println("*****");
+
+
+    }
+}
