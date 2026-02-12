f7d3b506841c452d3e149fa2e518ee28f87d2631
SerhiiKI13 <kugai.sirozha@gmail.com>
2026-02-12 14:28:47 +0100

    Update Main.java

--- src/lessons1/demo1/Main.java
+++ src/lessons1/demo1/Main.java
@@ -3,5 +3,7 @@ package lessons1.demo1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        List<String> string = new ArrayList<>();
+        string.add("hello");
     }
 }
